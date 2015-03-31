885415e89ec1cad1203fff387613c3883b4a7ce4
edoecohen <edoecohen@gmail.com>
2015-03-31 14:28:16 -0700

    Added Firebase

--- src/init.js
+++ src/init.js
@@ -1,7 +1,51 @@
 $(document).ready(function(){
+
   window.dancers = [];
 
+  var fb = new Firebase("https://fiery-fire-7499.firebaseio.com/Highscores");
+
+  var highscores;
+  var lowestHighscore = 1000;
+
+  fb.on('value', function(dataSnapshot) {
+    highscores = dataSnapshot.val();
+  });
+
+  var score = 0;
+  var timer = 5;
+
   var makers = [ 'makeDogDancer', 'makeFoxDancer', 'makeCalvinDancer', 'makeHannahDancer' ];
+
+  var timerId;
+  var countDown = function(){
+    timerId = setInterval(function(){
+      timer--;
+      $('.timer').text(timer);
+      if (timer === 0) {
+        alert('game over!');
+        clearInterval(timerId);
+        clearInterval(populate);
+
+
+        for(var key in highscores){
+          if (highscores[key] < lowestHighscore) {
+            lowestHighscore = highscores[key];
+            lowestHighscoreKey = key;
+          }
+        }
+        if (score > lowestHighscore){
+          fb.push({ "Jimmy": score });
+        }
+
+        console.log(lowestHighscoreKey + ": " + lowestHighscore);
+
+
+
+
+      }
+    }, 1000);
+  };
+  countDown();
 
   var autoPopulate = function(){
 
@@ -17,15 +61,18 @@ $(document).ready(function(){
     $('body').append(dancer.$node);
     var dancer =  makeDogDancer;
   };
-  setInterval(autoPopulate, 600);
+  var populate = setInterval(autoPopulate, 600);
 
 
   var onTarget = function(){
     $(document).on('click', '.dancer', function(e){
+      score++;
+      $('.score').html(score);
+      console.log(score);
       $(this).parent().find('.blood').show();
       setTimeout(function(){
         $('.blood').fadeOut();
-      }, 2000)
+      }, 2000);
 
       var interval = null;
       var counter = 0;
